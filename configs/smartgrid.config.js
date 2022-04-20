b443d61bb32fe2e9e33cad18df0a5a740437b8ae
iliagromov <ilia.workproffile@gmail.com>
2022-04-20 23:46:24 +0300

    added page

--- configs/smartgrid.config.js
+++ configs/smartgrid.config.js
@@ -3,11 +3,11 @@ const options = require('./breakPoints.json');
 module.exports = {
     outputStyle: 'sass', /* less || scss || sass || styl */
     columns: 12, /* number of grid columns */
-    offset: '18px', /* gutter width px || % || rem */
+    offset: '20px', /* gutter width px || % || rem */
     mobileFirst: true, /* mobileFirst ? 'min-width' : 'max-width' */
     container: {
-        maxWidth: '1920px', /* max-width оn very large screen */
-        fields: '18px' /* side fields */
+        maxWidth: '1100px', /* max-width оn very large screen */
+        fields: '20px' /* side fields */
     },
     breakPoints : options.breakPoints,
     // breakPoints: {
