9bcb84a572033a5cbbd97fab57568977b1437e47
iliagromov <ilia.workproffile@gmail.com>
2023-03-05 21:41:08 +0400

    added video

--- src/components/testimonials/testimonials.component.js
+++ src/components/testimonials/testimonials.component.js
@@ -2,8 +2,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const testimonialsStepsTabLinks = document.querySelectorAll('.testimonials-steps__nav .tab-link');
     const tabcontent = document.querySelectorAll('.testimonials-steps__contents .testimonials-steps__content');
-    
-    if (testimonialsStepsTabLinks && testimonialsStepsTabLinks.length !== 0 && tabcontent && tabcontent.length !==0) {
+
+    if (testimonialsStepsTabLinks && testimonialsStepsTabLinks.length !== 0 && tabcontent && tabcontent.length !== 0) {
         testimonialsStepsTabLinks.forEach((link) => {
             link.addEventListener('mouseover', (event) => {
                 [...testimonialsStepsTabLinks, ...tabcontent].forEach((link) => {
@@ -19,4 +19,42 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     }
+
+
+    const progressBar = document.querySelector("#progress-bar");
+    const playing = document.querySelector(".testimonials-slider__btn-progress-bar");
+    const timer = document.getElementById("timer");
+    const button = document.getElementById("play");
+    const video = document.querySelectorAll("video")[0];
+
+
+    function progressLoop() {
+        setInterval(function () {
+            let val =  Math.round((video.currentTime / video.duration) * 360);
+         
+            if(val > 0) {
+                progressBar.classList.add('testimonials-slider__btn_playing')
+            }
+            playing.style.background = `conic-gradient(#FF522C ${val}deg, transparent 0deg)`
+           
+            
+            timer.innerHTML = Math.round(video.currentTime) + " seconds";
+        });
+    }
+
+    function playPause() {
+        if (video.paused) {
+            video.play();
+            button.innerHTML = "&#10073;&#10073;";
+        } else {
+            video.pause();
+            button.innerHTML = "►";
+        }
+    }
+
+    button.addEventListener("click", playPause);
+    video.addEventListener("play", progressLoop);
+
+
+    
 });
