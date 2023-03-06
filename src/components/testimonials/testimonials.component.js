8bc5b661c114b1771b90b1b24793ec33cd75ca86
iliagromov <ilia.workproffile@gmail.com>
2023-03-06 12:07:18 +0400

    Закончил с видео

--- src/components/testimonials/testimonials.component.js
+++ src/components/testimonials/testimonials.component.js
@@ -21,40 +21,65 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
 
-    const progressBar = document.querySelector("#progress-bar");
-    const playing = document.querySelector(".testimonials-slider__btn-progress-bar");
-    const timer = document.getElementById("timer");
-    const button = document.getElementById("play");
-    const video = document.querySelectorAll("video")[0];
+    const videos = document.querySelectorAll(".page-video");
 
+    // FIXME: Баг с воспроизведением двух видео
+    // FIXME: убрать setInterval сделать watcher
+    // баг со свайпом на слайде
 
     function progressLoop() {
         setInterval(function () {
-            let val =  Math.round((video.currentTime / video.duration) * 360);
-         
-            if(val > 0) {
+            const progressBar = document.querySelector("#progress-bar");
+            const progressBarPlayingLine = document.querySelector(".testimonials-slider__btn-progress-bar");
+            const timer = document.getElementById("timer");
+            const video = document.querySelector(".swiper-slide-active .page-video");
+
+
+
+            let val = Math.round((video.currentTime / video.duration) * 360);
+
+            if (val > 0) {
+                console.log(val);
                 progressBar.classList.add('testimonials-slider__btn_playing')
             }
-            playing.style.background = `conic-gradient(#FF522C ${val}deg, transparent 0deg)`
-           
-            
+            progressBarPlayingLine.style.background = `conic-gradient(#FF522C ${val}deg, transparent 0deg)`
+
+
             timer.innerHTML = Math.round(video.currentTime) + " seconds";
         });
     }
 
-    function playPause() {
-        if (video.paused) {
-            video.play();
-            button.innerHTML = "&#10073;&#10073;";
-        } else {
-            video.pause();
-            button.innerHTML = "►";
-        }
-    }
+    // function playPause() {
 
-    button.addEventListener("click", playPause);
-    video.addEventListener("play", progressLoop);
+    // }
+
+    // button.addEventListener("click", playPause);
+    videos.forEach((video) => {
+        video.addEventListener("play", progressLoop);
+        video.addEventListener("play", function () {
+            // console.log('play');
+            video.muted = false;
+            this.parentNode.parentNode.classList.add('isActive');
+        });
+        video.addEventListener("pause", function () {
+            video.muted = true;
+            // console.log('pause');
+            this.parentNode.parentNode.classList.remove('isActive');
+        });
+        video.parentNode.querySelector('.page-btn').addEventListener('click', function () {
+            // console.log(this);
+                if (video.paused) {
+                    video.play();
+                    this.innerHTML = "&#10073;&#10073;";
+                } else {
+                    video.pause();
+                    this.innerHTML = "►";
+                }
+        });
+
+    });
 
 
-    
+
+
 });
